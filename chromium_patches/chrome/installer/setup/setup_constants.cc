afaf428cacd407ae787c557ce1c9efb58f5d3d2e
Nikhil Sonti <nikhilsv92@gmail.com>
2025-09-19 15:10:11 -0700

    fix: windows visual element manifest

---
+++ chromium_patches/chrome/installer/setup/setup_constants.cc
@@ -0,0 +1,13 @@
+diff --git a/chrome/installer/setup/setup_constants.cc b/chrome/installer/setup/setup_constants.cc
+index c3fa94531d449..8f4449ba75e64 100644
+--- a/chrome/installer/setup/setup_constants.cc
++++ b/chrome/installer/setup/setup_constants.cc
+@@ -10,7 +10,7 @@ namespace installer {
+ const wchar_t kChromeArchive[] = L"chrome.7z";
+ const wchar_t kChromeCompressedArchive[] = L"chrome.packed.7z";
+ const char kVisualElements[] = "VisualElements";
+-const wchar_t kVisualElementsManifest[] = L"chrome.VisualElementsManifest.xml";
++const wchar_t kVisualElementsManifest[] = L"browseros.VisualElementsManifest.xml";
+ 
+ // Sub directory of install source package under install temporary directory.
+ const wchar_t kInstallSourceDir[] = L"source";
